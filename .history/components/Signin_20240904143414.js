8048709f2135d7c7c43129425876aec2975cb7db
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2024-09-04 14:47:46 -0500

    worked on no auth page

---
+++ .history/components/Signin_20240904143414.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NoAuthNavBar from './NoAuthNavBar';
+
+function Signin() {
+  return (
+    <>
+      <NoAuthNavBar />
+      <div className="d-flex flex-wrap" />
+    </>
+  );
+}
+
+export default Signin;
---
+++ .history/components/Signin_20240904143527.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NoAuthNavBar from './NoAuthNavBar';
+
+function Signin() {
+  return (
+    <>
+      <NoAuthNavBar />
+      <div className="d-flex flex-wrap" />
+      <div className="full-width-block" />
+    </>
+  );
+}
+
+export default Signin;
