e1988a9baa168f7ff3757e34bceece84314d3713
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2024-09-17 13:34:06 -0500

    fixed create

--- pages/food_log/edit/[id].js
+++ pages/food_log/edit/[id].js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { getSingleFoodLog } from '../../../api/FoodLog';
 import FoodLogForm from '../../../components/forms/FoodLogForm';
+import { useAuth } from '../../../utils/context/authContext';
 
 export default function EditItem() {
   const [editObj, setEditObj] = useState(null);
   const router = useRouter();
   const { id } = router.query;
+  const { user } = useAuth(); 
 
   useEffect(() => {
     if (id) {
@@ -14,5 +16,5 @@ export default function EditItem() {
     }
   }, [id]);
 
-  return <FoodLogForm editObj={editObj} />;
+  return <FoodLogForm user={user} editObj={editObj} />;
 }
