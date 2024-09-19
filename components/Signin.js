d8b8f2ca6e1dd96adfe81ab1c2fdd13a07bc41f0
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2024-09-19 12:29:40 -0500

    css and code cleanup

--- components/Signin.js
+++ components/Signin.js
@@ -2,43 +2,26 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import NoAuthNavBar from './NoAuthNavBar';
 import { signIn } from '../utils/auth';
-import creatorProfileImage from '../assets/creator_profile_image.jpg'; 
+import creatorProfileImage from '../assets/creator_profile_image.JPG';
 
 function SignIn() {
-  
   return (
     <>
       <NoAuthNavBar />
       <div className="d-flex flex-wrap" />
-      <div className="full-width-block">
-        Welcome to Culinary Explorer! The best way to keep track of your dining experiences.
-      </div>
-      <div className="about-the-site-label">
-        ABOUT THE SITE
-      </div>
-      <div className="about-the-site">
-        Dining out can become a complex and often stressful experience for people with dietary restrictions, such as
-        allergies or intolerances. That is where Culinary Explorer steps in, it creates a single spot where you can log
-        the different restaurants you&apos;ve eaten at and your experience.
-      </div>
-      <div className="about-the-site-label">
-        ABOUT THE CREATOR
-      </div>
+      <div className="full-width-block">Welcome to Culinary Explorer! The best way to keep track of your dining experiences.</div>
+      <div className="about-the-site-label">ABOUT THE SITE</div>
+      <div className="about-the-site">Dining out can become a complex and often stressful experience for people with dietary restrictions, such as allergies or intolerances. That is where Culinary Explorer steps in, it creates a single spot where you can log the different restaurants you&apos;ve eaten at and your experience.</div>
+      <div className="about-the-site-label">ABOUT THE CREATOR</div>
       <div className="profile-container">
         <img src={creatorProfileImage.src} alt="Creator Profile" className="creator-profile-image" />
       </div>
-      <div className="about-the-site">
-        Before embarking on my software development journey, I worked as a Project Manager in the entertainment industry.
-        I enjoyed problem-solving and creating things that people would love. When I discovered software development,
-        I was immediately intrigued. I realized that the tech industry would enable me to continue utilizing the skills
-        I have honed such as planning, communication, and problem-solving. This sparked my passion for software, which has
-        only grown stronger with every discovery.
-      </div>
+      <div className="about-the-site">Before embarking on my software development journey, I worked as a Project Manager in the entertainment industry. I enjoyed problem-solving and creating things that people would love. When I discovered software development, I was immediately intrigued. I realized that the tech industry would enable me to continue utilizing the skills I have honed such as planning, communication, and problem-solving. This sparked my passion for software, which has only grown stronger with every discovery.</div>
       <div className="button-container">
-          <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
-            Sign In
-          </Button>
-        </div>
+        <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
+          Sign In
+        </Button>
+      </div>
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import { AuthProvider } from '../utils/context/authContext';
 import ViewDirectorBasedOnUserAuthStatus from '../utils/ViewDirector';
-import home from './index';
 
 function MyApp({ Component, pageProps }) {
   return (
