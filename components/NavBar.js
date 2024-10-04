0d051ac4c80a5fa2c7ec02691f9132f0d81aec68
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2024-10-04 15:59:25 -0500

    got the food map page working and displaying the map

--- components/NavBar.js
+++ components/NavBar.js
@@ -22,7 +22,7 @@ export default function NavBar() {
             <Link passHref href="/food_log/new">
               <Nav.Link as="a">Add To Food Log</Nav.Link>
             </Link>
-            <Link passHref href="/delete-me">
+            <Link passHref href="/food_map/food_map">
               <Nav.Link as="a">Food Map</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>
