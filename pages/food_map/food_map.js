137290fa3c38d3aa2d4103f7ec5a498e4ebb4c29
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2024-10-15 11:19:45 -0500

    got maps and map pins working

--- pages/food_map/food_map.js
+++ pages/food_map/food_map.js
@@ -1,48 +1,24 @@
-import React from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+// food_map.js
+import React, { useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import MapComponent from '../../api/GoogleMaps';
 
-const containerStyle = {
-  width: '1400px',
-  height: '800px',
-};
-
-const center = {
-  lat: 37.090240,
-  lng: -95.712891,
-};
-
-function MyComponent() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-  });
-
-  const [mapInstance, setMapInstance] = React.useState(null); // eslint-disable-line no-unused-vars
-
-  const onLoad = React.useCallback((map) => {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    setMapInstance(map);
+const App = () => {
+  useEffect(() => {
+    console.log('useEffect called');
+    if (!window.root) {
+      const appElement = document.getElementById('app');
+      if (appElement) {
+        window.root = createRoot(appElement);
+        window.root.render(<MapComponent />);
+        console.log('MapComponent rendered');
+      } else {
+        console.error('No element with id "app" found');
+      }
+    }
   }, []);
 
-  const onUnmount = React.useCallback((map) => { // eslint-disable-line no-unused-vars
-    setMapInstance(null);
-  }, []);
+  return <div id="app" />;
+};
 
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={4}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      { /* Child components, such as markers, info windows, etc. */ }
-      <></>
-    </GoogleMap>
-  ) : <></>;
-}
-
-export default React.memo(MyComponent);
+export default App;
