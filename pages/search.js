738928e437cddaa13cc9d5d139efea7a3a49e88e
Jesse Ramirez <gedeon.ram.dev@gmail.com>
2024-10-24 12:07:35 -0500

    got search routing the user to the card appropriatly

--- pages/search.js
+++ pages/search.js
@@ -1,63 +1,8 @@
-// import React, { useState, useEffect } from 'react';
-// import PropTypes from 'prop-types';
-// import Loading from '../components/Loading';
-// import FoodLogCard from '../components/FoodLogCard';
-
-// const Search = ({ query }) => {
-//   const [items, setItems] = useState([]);
-//   const [loading, setLoading] = useState(false);
-
-//   useEffect(() => {
-//     if (query) {
-//       setLoading(true);
-//       console.log('Fetching search results for:', query); // Log the query
-
-//       fetch(`/api/search?query=${query}`)
-//         .then((response) => response.json())
-//         .then((data) => {
-//           console.log('Search results:', data); // Log the search results
-//           setItems(data);
-//           setLoading(false);
-//         })
-//         .catch((error) => {
-//           console.error('Error fetching search results:', error);
-//           setLoading(false);
-//         });
-//     } else {
-//       setLoading(false);
-//     }
-//   }, [query]);
-
-//   if (loading) {
-//     return <Loading />;
-//   }
-
-//   return (
-//     <div className="search-results">
-//       {query && items.length === 0 ? (
-//         <p>No items found for &quot;{query}&quot;.</p>
-//       ) : (
-//         <div className="d-flex flex-wrap justify-content-evenly">
-//           {items.map((item) => (
-//             <FoodLogCard key={item.id} itemObj={item} onUpdate={() => {}} />
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// Search.propTypes = {
-//   query: PropTypes.string.isRequired,
-// };
-
-// export default Search;
-// pages/search.js
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
-import Loading from '../components/Loading'; // Adjust the path as necessary
-import FoodLogCard from '../components/FoodLogCard'; // Adjust the path as necessary
 import { searchItems } from '../api/FoodLog';
+import Loading from '../components/Loading';
+import FoodLogCard from '../components/FoodLogCard';
 
 const Search = () => {
   const router = useRouter();
@@ -81,6 +26,10 @@ const Search = () => {
     }
   }, [query]);
 
+  const handleItemClick = (id) => {
+    router.push(`/food_log/${id}`);
+  };
+
   if (loading) {
     return <Loading />;
   }
@@ -92,7 +41,11 @@ const Search = () => {
       ) : (
         <div className="d-flex flex-wrap justify-content-evenly">
           {items.map((item) => (
-            <FoodLogCard key={item.id} itemObj={item} onUpdate={() => {}} />
+            <FoodLogCard
+              key={item.id}
+              itemObj={item}
+              onClick={() => handleItemClick(item.id)}
+            />
           ))}
         </div>
       )}
